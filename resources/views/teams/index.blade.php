069897b19f6a6d997d8ce9decb73c4cfe3f57747
igor3m <francisco.souza1102@aluno.ce.gov.br>
2026-05-25 09:25:05 -0300

    fiz o index e o css

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,6 +1,58 @@
-<div>
-    <p>o nome do time é </p>
-    @foreach ($teams as $team) 
-        <p>o seu time é {{ $team->name }}</p>
-    @endforeach
+@extends('layouts.app')
+
+@section('content')
+
+@vite('resources/css/app.css')
+
+<header>
+
+@vite('resources/css/app.css')
+
+  
+
+    <div class="logo">⚽ <span>TIME</span> CONECTADO</div>
+
+    <nav>
+        <a href="#">Início</a>
+        <a href="#">Times</a>
+        <a href="#">Jogos</a>
+        <a href="#">Campeonatos</a>
+        <a href="#">Jogadores</a>
+        <a href="#">Notícias</a>
+        <a href="#">Contato</a>
+    </nav>
+
+    <button class="btn-topo">Cadastre seu time</button>
+</header>
+
+<section class="hero">
+    <div class="conteudo">
+        <h1>Mais que um site,<br><span>uma paixão!</span></h1>
+
+        <p>
+            Cadastre seu time, encontre adversários,
+            participe de campeonatos e mostre a força do seu futebol.
+        </p>
+
+        <button class="btn-principal">Faça parte</button>
+    </div>
+</section>
+
+<!-- LISTA DE TIMES (SEU CÓDIGO) -->
+<div class="times-box">
+    <h2>⚽ Times cadastrados</h2>
+
+    @if ($teams->isEmpty())
+        <p>Nenhum time cadastrado.</p>
+    @else
+        <div class="lista-times">
+            @foreach ($teams as $team) 
+                <div class="time-card">
+                    {{ $team->name }}
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
+
+@endsection
